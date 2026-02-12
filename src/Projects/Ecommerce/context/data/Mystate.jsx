4029af68bfb4fdc5880afedb64313f5be58b595e
Ullas MP <ullasmpu@gmail.com>
2026-02-12 18:11:01 +0530

    Mycontext bug cleared

--- src/Projects/Ecommerce/context/data/Mystate.jsx
+++ src/Projects/Ecommerce/context/data/Mystate.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import myContext from './myContext'
+import myContext from "@/Projects/Ecommerce/context/data/myContext";
+
 import { Timestamp } from 'firebase/firestore'
 import {
   collection,
