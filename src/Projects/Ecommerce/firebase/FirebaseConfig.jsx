1738409f8b27e083aef3edcba81df41198bbbe4d
Ullas MP <ullasmpu@gmail.com>
2026-02-13 09:35:58 +0530

    FireBase auth error Debuged

--- src/Projects/Ecommerce/firebase/FirebaseConfig.jsx
+++ src/Projects/Ecommerce/firebase/FirebaseConfig.jsx
@@ -4,10 +4,22 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Load Firebase config from GitHub secret
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+console.log("RAW ENV:", import.meta.env.VITE_FIREBASE_CONFIG);
+
+let firebaseConfig;
+
+try {
+  firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+  console.log("PARSED CONFIG:", firebaseConfig);
+} catch (e) {
+  console.error("JSON PARSE FAILED:", e);
+}
+
+const app = initializeApp(firebaseConfig);
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
 
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
